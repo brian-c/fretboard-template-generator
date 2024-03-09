ec5faa5ccf24f237469888932097ae625324d1a7
Brian Carstensen <brian.carstensen@gmail.com>
2024-03-09 11:18:38 -0600

    Add details to registration mark

--- src/registration-mark.tsx
+++ src/registration-mark.tsx
@@ -17,11 +17,13 @@ interface Props {
 export default function RegistrationMark({ size, unit }: Props) {
   return (
     <g {...styles.registrationMark}>
-      <circle
-        cx={`${size / 2}${unit}`}
-        cy={`${size / 2}${unit}`}
-        r={`${size / 3}${unit}`}
-      />
+      {Array(5).fill(null).map((n, i, a) => (
+        <circle
+          cx={`${size / 2}${unit}`}
+          cy={`${size / 2}${unit}`}
+          r={`${(size - size / a.length * i) / 2.1}${unit}`}
+        />
+      ))}
 
       <line
         x1="0"
@@ -35,6 +37,20 @@ export default function RegistrationMark({ size, unit }: Props) {
         y1={`${size}${unit}`}
         x2={`${size}${unit}`}
         y2="0"
+      />
+
+      <line
+        x1={`${size / 2}${unit}`}
+        y1={0}
+        x2={`${size / 2}${unit}`}
+        y2={`${size}${unit}`}
+      />
+
+      <line
+        x1={0}
+        y1={`${size / 2}${unit}`}
+        x2={`${size}${unit}`}
+        y2={`${size / 2}${unit}`}
       />
     </g>
   );
